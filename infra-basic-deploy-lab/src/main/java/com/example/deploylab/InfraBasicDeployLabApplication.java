321f0ed8c553b435422ce47084858b961cac5395
zionn1002 <zionn1002@naver.com>
2026-03-23 09:44:34 +0900

    add infra-basic-deploy-lab

---
+++ infra-basic-deploy-lab/src/main/java/com/example/deploylab/InfraBasicDeployLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraBasicDeployLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraBasicDeployLabApplication.class, args);
+	}
+
+}
---
+++ infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
@@ -0,0 +1,15 @@
+package com.example.deploylab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BackendController {
+
+    @GetMapping("/backend")
+    public String backend() {
+        return "backend";
+    }
+}
